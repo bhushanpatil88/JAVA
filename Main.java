e38b9da052dc7e8deaf564af6d61c79dfd8a05cf
Bhushan <76675802+bhushanpatil88@users.noreply.github.com>
2025-02-17 22:09:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,163 @@
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+
+
+public class Main
+{
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    
+    public static void solve() throws Exception{
+            int n = in.nextInt();
+            String s = in.nextLine();
+            int [] arr = in.readArr(n);
+            print(arr);
+            out.print(s);
+    }
+    
+    public static void main(String hi[]) throws Exception
+    {
+        try {
+            
+            int T=in.nextInt();
+            while(T-- > 0){
+                solve();
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+        
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArr(int N){
+            int [] arr = new int[N];
+            for(int i = 0;i<N;i++)
+                arr[i] = in.nextInt();
+            return arr;
+        }
+    }
+
+    public static void print(int[] arr)
+    {
+        for(int x: arr)
+            out.print(x+" ");
+        out.println();
+    }
+    public static boolean isPrime(long n)
+    {
+        if(n < 2) return false;
+        if(n == 2 || n == 3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        long sqrtN = (long)Math.sqrt(n)+1;
+        for(long i = 6L; i <= sqrtN; i += 6) {
+            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+        }
+        return true;
+    }
+    public static long gcd(long a, long b)
+    {
+        if(a > b)
+            a = (a+b)-(b=a);
+        if(a == 0L)
+            return b;
+        return gcd(b%a, a);
+    }
+    public static long totient(long n)
+    {
+        long result = n;
+        for (int p = 2; p*p <= n; ++p)
+            if (n % p == 0)
+            {
+                while(n%p == 0)
+                    n /= p;
+                result -= result/p;
+            }
+        if (n > 1)
+            result -= result/n;
+        return result;
+    }
+    public static ArrayList<Integer> findDiv(int N)
+    {
+        ArrayList<Integer> ls1 = new ArrayList<Integer>();
+        ArrayList<Integer> ls2 = new ArrayList<Integer>();
+        for(int i=1; i <= (int)(Math.sqrt(N)+0.00000001); i++)
+            if(N%i == 0)
+            {
+                ls1.add(i);
+                ls2.add(N/i);
+            }
+        Collections.reverse(ls2);
+        for(int b: ls2)
+            if(b != ls1.get(ls1.size()-1))
+                ls1.add(b);
+        return ls1;
+    }
+    public static void sort(int[] arr)
+    {
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static long power(long x, long y, long p)
+    {
+        long res = 1L;
+        x = x%p;
+        while(y > 0)
+        {
+            if((y&1)==1)
+                res = (res*x)%p;
+            y >>= 1;
+            x = (x*x)%p;
+        }
+        return res;
+    }
+       
+}
+
+
